c3a33e53ad2cd311ba6e605995a694cd19f714d9
Jerome Guarin <Jerome.Guarin@navitaire.com>
2022-05-02 18:46:33 +1000

    added useState for cart displaying

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,17 +1,32 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import './App.css';
 import Cart from './components/Cart/Cart';
 import Header from './components/Header/Header';
 import Meals from './components/Meals/Meals';
 
 function App() {
+  const [isCartShown, setIsCartShown] = useState(false);
+
+  const displayCartHandler = (event: Event) => {
+    // setIsCartShown((current) => {
+    //   return false;
+    // });
+    setIsCartShown(true);
+  };
+
+  function hideCartHandler(event: Event) {
+    setIsCartShown(false);
+  }
+
   return (
     <Fragment>
-      <Cart
-        onCloseButtonClick={() => {}}
-        onModalBackdropClick={() => {}}
-      />
-      <Header onHeaderButtonClick={() => {}} />
+      {isCartShown && (
+        <Cart
+          onCloseButtonClick={hideCartHandler}
+          onModalBackdropClick={hideCartHandler}
+        />
+      )}
+      <Header onHeaderButtonClick={displayCartHandler} />
       <Meals />
     </Fragment>
   );
