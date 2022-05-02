5228ff463a4b55d1062b7a16ae2fa044069c858c
Jerome Guarin <Jerome.Guarin@navitaire.com>
2022-05-02 12:26:50 +1000

    Meal summary, on Meals component

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
+import Meals from './components/Meals/Meals';
 
 function App() {
-  return <Header onHeaderButtonClick={() => {}} />;
+  return (
+    <Fragment>
+      <Header onHeaderButtonClick={() => {}} />
+      <Meals></Meals>
+    </Fragment>
+  );
 }
 
 export default App;
