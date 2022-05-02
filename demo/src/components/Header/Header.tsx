9106ebad7c6cd2a6e927c3c347cf86eba8af8898
Jerome Guarin <Jerome.Guarin@navitaire.com>
2022-05-02 11:45:24 +1000

    add the header an image

---
+++ demo/src/components/Header/Header.tsx
@@ -0,0 +1,25 @@
+import { FC, Fragment } from 'react';
+import mealsImage from '../../assets/meals.jpg';
+import classes from './Header.module.css';
+
+interface HeaderProps {
+  onHeaderButtonClick: (event: any) => any;
+}
+
+const Header: FC<HeaderProps> = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+      </header>
+      <div className={classes['main-image']}>
+        <img
+          src={mealsImage}
+          alt='Table full of food.'
+        />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
