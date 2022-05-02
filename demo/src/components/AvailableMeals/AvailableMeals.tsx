81139b795caf6efc20ce3731aff8e165ec65ca06
Jerome-MP <jerome.guarin@navitaire.com>
2022-05-02 21:33:29 +1000

    add useEffect on available meals

--- demo/src/components/AvailableMeals/AvailableMeals.tsx
+++ demo/src/components/AvailableMeals/AvailableMeals.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import DUMMY_MEALS from '../../data/dummy-meals';
+import React, { useEffect, useState } from 'react';
+import fireBaseCofig from '../../config/firebase.json';
+import { MealProps } from '../../interfaces/meal';
 import Card from '../Card/Card';
 import classes from './AvailableMeals.module.css';
 import MealItem from './MealItem';
@@ -7,12 +8,26 @@ import MealItem from './MealItem';
 type Props = {};
 
 const AvailableMeals = (props: Props) => {
-  const meals = DUMMY_MEALS.map((meal) => (
+  const [mealsData, setMealsData] = useState<MealProps[]>([]);
+
+  const fetchMeals = async () => {
+    const response = await fetch(fireBaseCofig.url);
+    const json = (await response.json()) as MealProps[];
+
+    setMealsData(json);
+  };
+
+  useEffect(() => {
+    fetchMeals();
+  }, []);
+
+  const meals = mealsData.map((meal) => (
     <MealItem
       key={meal.id}
       {...meal}
     />
   ));
+
   return (
     <section className={classes.meals}>
       <Card>
